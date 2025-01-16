ca89801bf3d500ee02b08ae21407bdacbe2dfb36
Kamilla Huhnke <kamillahuhnke@me.com>
2025-01-16 15:31:19 +0100

    føds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "24 Dec 2024";
+const newYears = "13 Apr 2025";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
